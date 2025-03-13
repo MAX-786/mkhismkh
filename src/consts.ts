9508910a47af7f513e20805477edc5528829ad0c
Mohammad K. Hussain <hussainmohammad328@gmail.com>
2025-03-14 04:22:10 +0530

    setup astro project

---
+++ src/consts.ts
@@ -0,0 +1,23 @@
+import type { Site, Links } from "@/types";
+
+export const SITE: Site = {
+  TITLE: "MKH",
+  DESCRIPTION: "MAX-786's personal website",
+  AUTHOR: "MAX-786",
+  REPO_URL: "https://github.com/MAX-786/MAX-786.github.io",
+};
+
+export const LINKS: Links = [
+  {
+    TEXT: "Home",
+    HREF: "/",
+  },
+  {
+    TEXT: "About",
+    HREF: "/about",
+  },
+  {
+    TEXT: "Projects",
+    HREF: "/projects",
+  },
+];
