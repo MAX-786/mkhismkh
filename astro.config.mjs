e5eb8c0b2f8e23894100a07846896db9bb95a6ee
Mohammad K. Hussain <hussainmohammad328@gmail.com>
2025-03-15 20:57:53 +0530

    setup vercel ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mkhismkh.com",
   integrations: [react(), tailwind(), icon()],
-
-  adapter: node({
-    mode: "standalone",
-  }),
+  output: "hybrid",
+  adapter: vercel(),
 });
