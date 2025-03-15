3d19497f7f8c1dafe300df92c4981aa85689bb5b
Mohammad K. Hussain <hussainmohammad328@gmail.com>
2025-03-15 06:45:12 +0530

    ssg to ssr in garden pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,14 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://mkhismkh.com",
   integrations: [react(), tailwind(), icon()],
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
